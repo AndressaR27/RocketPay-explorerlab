2e8ec699211adcdcfab0945721a73673307134eb
AndressaR <dre.rodrigues27@gmail.com>
2022-10-24 12:47:50 -0300

    Add new card - Amex

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ function changeCardType(type) {
     mastercard: ["#DF6F29", "#C69347"],
     default: ["black", "gray"],
     elo: ["#5CCDFE", "#FF4D4D"],
+    amex: ["#5CCDFE", "#7C4DFF"],
   }
 
   ccBgColor01.setAttribute("fill", colors[type][0])
@@ -61,6 +62,11 @@ const cardNumberPattern = {
       mask: "0000 0000 0000 0000",
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard",
+    },
+    {
+      mask: "0000 000000 00000",
+      regex: /^3[47]\d{0,13}/,
+      cardtype: "amex",
     },
     {
       mask: "0000 0000 0000 0000",
